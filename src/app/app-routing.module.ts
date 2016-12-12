1279b9c10dfdaa3804221d4e86c222e29f7a7a52
vadimstrukov <vadim.strukov@gmail.com>
2016-12-12 17:36:54 +0200

    Deleted hashRouting due to NGINX server implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   {path: 'library', component: UserBooks, canActivate: [CanActivateViaAuth]}
 ];
 @NgModule({
-  imports: [ RouterModule.forRoot(routes, {useHash: true}) ],
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule{}
