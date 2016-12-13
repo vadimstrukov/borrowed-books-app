4d08f331cf43c90eff592b35a47e9d6dcc1702bf
vadimstrukov <vadim.strukov@gmail.com>
2016-12-13 21:25:24 +0200

    added expanded status for user's book

--- src/app/directives/userbooks.directive.ts
+++ src/app/directives/userbooks.directive.ts
@@ -12,10 +12,29 @@ import {OwnedBook} from "../model/OwnedBook";
 
 export class UserBooks implements OnInit{
   userBooks:Array<OwnedBook>;
+  selectedUserBook:OwnedBook;
+  statusExpanded:boolean = false;
 
   constructor(private bookService:BookService){}
 
   ngOnInit():void{
     this.bookService.getUserBooks().subscribe(data=>this.userBooks = data);
+  }
+
+  private selectBook(book:OwnedBook, expandable:boolean){
+    this.selectedUserBook = book;
+    this.statusExpanded = expandable;
+  };
+
+  public bookClicked(book:OwnedBook){
+    switch (this.selectedUserBook){
+      case book:
+        this.selectBook(null, false);
+        console.log("Close expandable, Old Book", this.selectedUserBook);
+        break;
+      default:
+        this.selectBook(book, true);
+        console.log("Open expandable, New Book", this.selectedUserBook);
+    }
   }
 }
