640c023a453959ba2db355e814fa394392112b51
vadimstrukov <vadim.strukov@gmail.com>
2016-12-20 14:41:51 +0200

    updated Constants

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -5,12 +5,12 @@ export class Constants{
   private static get API_URL():string {return 'https://books.strukov.eu/api/books'}
   public static get GoogleAPI():string {return 'https://www.googleapis.com/books/v1/volumes'}
   public static get OAuthURL():string {return this.API_URL + '/oauth/token'}
-  public static get LogoutURL():string {return this.API_URL + '/user/logout'}
-  public static get LoggedInUser():string{return this.API_URL + '/user/me'}
+  public static get LogoutURL():string {return this.API_URL + '/v1/user/logout'}
+  public static get LoggedInUser():string{return this.API_URL + '/v1/user/me'}
   public static get Register():string{return this.API_URL + '/register'}
-  public static get Books():string{return this.API_URL + '/owned'}
+  public static get Books():string{return this.API_URL + '/v1/owned'}
   public static get API_KEY():string {return 'AIzaSyBfeYjfXYq_kccfOb-wRPnRw5f_ED5hpaA'}
-  public static get CheckOwnedBook():string {return this.API_URL + '/owned/check'}
+  public static get CheckOwnedBook():string {return this.API_URL + '/v1/owned/check'}
   public static get LoginModal():JQuery {return $('#login') }
   public static get BookInfoModal():JQuery {return $('#bookinfo') }
 }
