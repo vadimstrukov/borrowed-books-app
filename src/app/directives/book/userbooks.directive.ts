2594b3cddafb46b5bd87884b69dd8c1bb690f632
Dastas <stanislav.kosharinski@gmail.com>
2016-12-23 02:18:45 +0200

    redesigned user's book's cards

--- src/app/directives/book/userbooks.directive.ts
+++ src/app/directives/book/userbooks.directive.ts
@@ -9,10 +9,23 @@ import {Book} from "../../model/Book";
 import {BookInfoModal} from "./bookinfo.directive";
 import {FormGroup, FormBuilder} from "@angular/forms";
 import {Options} from "./radio.options";
+import {Input, trigger, state, style, transition, animate} from '@angular/core';
 
 @Component({
   templateUrl: 'userbooks.html',
-  styleUrls: ['../../app.component.scss']
+  styleUrls: ['../../app.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({opacity: '1', '-webkit-transform': 'none', transform: 'none'})),
+      transition('void => *', [
+        style({opacity: '0', '-webkit-transform': 'translate3d(0, 100%, 0)', transform: 'translate3d(0, 100%, 0)'}),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({opacity: '0', '-webkit-transform': 'translate3d(0, 100%, 0)', transform: 'translate3d(0, 100%, 0)'}))
+      ])
+    ])
+  ]
 })
 
 export class UserBooks implements OnInit{
