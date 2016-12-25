35b8c5ba8ec495f2305b1fd01a60f4deb972f61f
vadimstrukov <vadim.strukov@gmail.com>
2016-12-25 22:25:17 +0200

    Updated urls mapping

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -8,9 +8,12 @@ export class Constants{
   public static get LogoutURL():string {return this.API_URL + '/api/v1/user/logout'}
   public static get LoggedInUser():string{return this.API_URL + '/api/v1/user/me'}
   public static get Register():string{return this.API_URL + '/api/v1/register'}
-  public static get Books():string{return this.API_URL + '/api/v1/books/owned'}
+  public static get OwnedBooks():string{return this.API_URL + '/api/v1/books/owned'}
+  public static get BorrowedBooks():string{return this.API_URL + '/api/v1/books/borrowed'}
   public static get API_KEY():string {return 'AIzaSyBfeYjfXYq_kccfOb-wRPnRw5f_ED5hpaA'}
   public static get CheckOwnedBook():string {return this.API_URL + '/api/v1/books/owned/check'}
   public static get LoginModal():JQuery {return $('#login') }
   public static get BookInfoModal():JQuery {return $('#bookinfo') }
+  public static get BorrowBookModal():JQuery {return $('#borrowbook') }
+
 }
