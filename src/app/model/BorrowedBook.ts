c4bf991fc51dbfd82e440c5b5761d65154d60b89
vadimstrukov <vadim.strukov@gmail.com>
2016-12-28 16:34:51 +0200

    Added id to BorrowedBook

--- src/app/model/BorrowedBook.ts
+++ src/app/model/BorrowedBook.ts
@@ -3,6 +3,7 @@ import {OwnedBook} from "./OwnedBook";
  * Created by strukov on 23.12.16.
  */
 export class BorrowedBook{
+  id?: number;
   ownedBook: OwnedBook;
   borrowDate: Date;
   returnDate: Date;
