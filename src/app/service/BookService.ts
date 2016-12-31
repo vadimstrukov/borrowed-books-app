474c96c91ae21bc9b068c3ed72a44855973c3c8c
vadimstrukov <vadim.strukov@gmail.com>
2016-12-31 02:26:55 +0200

    Implemented missing generics. ForkJoin methods now use generics base methods

--- src/app/service/BookService.ts
+++ src/app/service/BookService.ts
@@ -1,24 +1,26 @@
 import {Injectable} from "@angular/core";
 import {Http, URLSearchParams, Response} from "@angular/http";
 import {Constants} from "../utils/Constants";
-import 'rxjs/Rx';
+import "rxjs/Rx";
 import {Book} from "../model/Book";
 import {Authentication} from "../utils/Authentication";
-import {OwnedBook} from "../model/OwnedBook";
-import {Observable, Subscription} from "rxjs";
+import {Observable} from "rxjs";
 import {BookItems} from "../model/BookItems";
 import {BorrowedBook} from "../model/BorrowedBook";
 import {ErrorObservable} from "rxjs/observable/ErrorObservable";
+import {OwnedBook} from "../model/OwnedBook";
 /**
  * Created by strukov on 15.11.16.
  */
 
 @Injectable()
-export class BookService{
+export class BookService {
   public startIndex: any;
-  constructor(private http:Http, private auth:Authentication){}
 
-  public getBooksByTitle(title:string):Observable<BookItems>{
+  constructor(private http: Http, private auth: Authentication) {
+  }
+
+  public getBooksByTitle(title: string): Observable<BookItems> {
     let params = new URLSearchParams();
     params.set('q', title);
     params.set('startIndex', this.startIndex);
@@ -26,73 +28,74 @@ export class BookService{
     params.set('filter', 'partial');
     params.set('projection', 'lite');
     params.set('key', Constants.API_KEY);
-    return this.http.get(Constants.GoogleAPI, {search: params}).map(response => response.json());
+    return this.http.get(Constants.GoogleAPI, {search: params}).map(response=>response.json());
   }
 
-  public getBookWithCheck(id:string):Observable<any>{
+  public getBookWithCheck(id: string): Observable<any> {
     return Observable.forkJoin(
-      this.http.get(Constants.GoogleAPI + '/' + id)
-        .map(response => response.json()),
+      this.getItem(id, Constants.GoogleAPI),
       this.http.get(Constants.CheckOwnedBook, {search: setIdURLParam(id), headers: this.auth.setAuthHeaders()})
-        .map(response=>response.json())
+        .map(response => response.json())
+    ).catch(handleError);
+  }
+
+  public getBookWithoutCheck(id: string): Observable<Book> {
+    return this.getItem(id, Constants.GoogleAPI);
+  }
+
+  public borrowBook(borrowedBook: BorrowedBook): Observable<any> {
+    return Observable.forkJoin(
+      this.saveItem(borrowedBook, Constants.BorrowedBooks),
+      this.updateItem(borrowedBook.ownedBook, Constants.OwnedBooks)
     );
   }
 
-  public getBookWithoutCheck(id:string):Observable<Book>{
-    return this.http.get(Constants.GoogleAPI + '/' + id).map(response=>response.json());
-  }
-
-  public saveBook(ownedBook:OwnedBook):Observable<OwnedBook>{
-    return this.http.post(Constants.OwnedBooks, JSON.stringify(ownedBook), {headers: this.auth.setAuthHeaders()})
-      .map(response => response.json());
-  }
-
-  public borrowBook(borrowedBook:BorrowedBook):Observable<any>{
+  public returnBook(borrowedBook: BorrowedBook) {
     return Observable.forkJoin(
-      this.http.post(Constants.BorrowedBooks, JSON.stringify(borrowedBook), {headers: this.auth.setAuthHeaders()})
-        .map(response => response.json()),
-      this.http.put(Constants.OwnedBooks, JSON.stringify(borrowedBook.ownedBook), {headers: this.auth.setAuthHeaders()})
-        .map(response => response.json()));
+      this.updateItem(borrowedBook.ownedBook, Constants.OwnedBooks),
+      this.deleteItem(borrowedBook.id, Constants.BorrowedBooks)
+    );
   }
 
-  public returnBook(borrowedBook:BorrowedBook):Subscription{
-    return Observable.forkJoin(
-      this.http.delete(Constants.BorrowedBooks, {search: setIdURLParam(borrowedBook.id.toString()), headers: this.auth.setAuthHeaders()}),
-      this.http.put(Constants.OwnedBooks, JSON.stringify(borrowedBook.ownedBook), {headers: this.auth.setAuthHeaders()})
-    ).subscribe(()=>{
-      deleteBookFromMem(borrowedBook, this.borrowedBooks);
-    });
+  public saveItem<T>(item: T, url: string): Observable<Response> {
+    return this.http.post(url, JSON.stringify(item), {headers: this.auth.setAuthHeaders()})
+      .catch(handleError);
   }
 
-  public deleteItem<T>(id:string, url:string):Observable<T>{
+  public deleteItem<T>(id: any, url: string): Observable<T> {
     return this.http.delete(url, {search: setIdURLParam(id), headers: this.auth.setAuthHeaders()})
       .catch(handleError);
   }
 
-  public getItems<T>(url:string):Observable<any>{
+  public getItems<T>(url: string) {
     return this.http.get(url, {headers: this.auth.setAuthHeaders()})
-      .map(response=>response.json())
+      .map(response => response.json())
       .catch(handleError);
   }
 
-  public updateItem<T>(item:T, url:string): Observable<T>{
+  public getItem<T>(id: any, url: string): Observable<any> {
+    return this.http.get(url + '/' + id)
+      .map(response => response.json()).catch(handleError);
+  }
+
+  public updateItem<T>(item: T, url: string): Observable<T> {
     return this.http.put(url, JSON.stringify(item), {headers: this.auth.setAuthHeaders()})
       .catch(handleError);
   }
 }
 
-function handleError (error: any): ErrorObservable {
+function handleError(error: any): ErrorObservable {
   let errorMsg = error.message || 'There was a problem with our API, try again...';
   console.error(errorMsg);
   return Observable.throw(errorMsg);
 }
 
-function setIdURLParam(id:string): URLSearchParams{
+function setIdURLParam(id: any): URLSearchParams {
   let params = new URLSearchParams();
-  params.set("id", id);
+  params.set("id", id.toString());
   return params;
 }
 
-export function deleteBookFromMem<T>(book:T, array:Array<T>):void{
+export function deleteBookFromMem<T>(book: T, array: Array<T>): void {
   array.splice(array.indexOf(book), 1);
 }
